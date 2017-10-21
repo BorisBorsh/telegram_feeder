d2f60b12f569eccb995896153d97e1e53ea65cdb
pi <Nonexisted@mail.com>
2017-10-21 16:24:46 +0300

    Corrected servo.py module

--- servo.py
+++ servo.py
@@ -3,9 +3,6 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 
-SERVO_PIN_BOARD_NUMBER = 11
-
-
 class Servo:
     """Define a context menager Servo"""
     def __init__(self, SERVO_PIN_BOARD_NUMBER):
@@ -14,14 +11,14 @@ class Servo:
     def __enter__(self):
         # Set up GPIO
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(SERVO_PIN_BOARD_NUMBER, GPIO.OUT)
+        GPIO.setup(self.SERVO_PIN_BOARD_NUMBER, GPIO.OUT)
 
     def __exit__(self, type, value, tracebac):
         # Clean all GPIO pins settings
         GPIO.cleanup()
 
 
-def servo_rotate(servo_rotate_time_sec=3):
+def servo_rotate(SERVO_ROTATE_TIME_SEC, SERVO_PIN_BOARD_NUMBER):
     """Rotate servo to dispence food"""
     PWM_FREQ_HZ = 50
     PWM_START_DUTY_CYCLE = 0
@@ -33,14 +30,15 @@ def servo_rotate(servo_rotate_time_sec=3):
     
     # Rorate servo
     pwm.ChangeDutyCycle(PWM_ROTATE_DUTY_CYCLE)
-    sleep(servo_rotate_time_sec)
+    sleep(SERVO_ROTATE_TIME_SEC)
     pwm.stop()
     print('A portion of food was added.')
 
 
 def feed_pet():
+    SERVO_ROTATE_TIME_SEC = 3
+    SERVO_PIN_BOARD_NUMBER = 11
     """Controlling servo to feed pets"""
     with Servo(SERVO_PIN_BOARD_NUMBER):
-        servo_rotate()
-
+        servo_rotate(SERVO_ROTATE_TIME_SEC, SERVO_PIN_BOARD_NUMBER)
 
